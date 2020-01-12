3f2ad79392286a51cf391d2f33a5e0b7db3b04c8
waverly hinton <bobalink13@gmail.com>
2020-01-12 13:15:04 -0700

    timestamps, User IDs and join and leave messages.

--- src/clientApp.js
+++ src/clientApp.js
@@ -1,15 +1,40 @@
 
 const socket = io();
 
-$('form').submit(function(e){
+const addToChatField = (chatEvent) => {
+  const message = $('<li class = "list-group-item"></li>');
+  const timestamp = new Date(Date.now()).toLocaleString();
+  const idString = chatEvent.userId? chatEvent.userId + ': ' : '';
+  message.append('<strong>' + idString + timestamp + '</strong>');
+  message.append('<p>' + chatEvent.text + '</p>');
+  $('#messages').append(message);
+};
+
+$('form').submit((e) =>{
   e.preventDefault(); // prevents page reloading
-  socket.emit('chat message', $('#messagebox').val());
-  console.log('===> id: ', socket.id, "message:", $('#messagebox').val());
+  const text = $('#messagebox').val();
+  if (text === ''){
+    return;
+  }
+  socket.emit('chat message', { text , userId: socket.id });
+  console.log('===> id: ', socket.id, 'message:', $('#messagebox').val());
   $('#messagebox').val('');
-  return false;
 });
 
-socket.on('chat message', function(msg){
-  console.log('<=== id: ', socket.id, "message:", msg );
-  $('#messages').append($('<li>').text(msg));
+socket.on('chat message', (chatEvent) =>{
+  console.log('<=== id: ', chatEvent.userId, 'message:', chatEvent.text );
+  addToChatField(chatEvent);
+});
+
+socket.on('user joined', (id)=> {
+  if(id === socket.id) {
+    addToChatField({ text :`Joined chat as user ${id}`});
+  }
+  else{
+    addToChatField({ text: `User ${id} joined the chat`});
+  }
+});
+
+socket.on('user disconnected', (id)=> {
+  addToChatField({ text:`User ${id} disconnected` });
 });
--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ function create(http) {
   const io = require('socket.io')(http);
 
   io.on('connection', function(socket){
+    io.emit('user joined', socket.id);
     console.log('a user connected', socket.id);
     socket.on('disconnect', function() {
-      console.log('user disconnected');
+      io.emit('user disconnected', socket.id);
     });
     socket.on('chat message', function(msg){
       io.emit('chat message', msg);
